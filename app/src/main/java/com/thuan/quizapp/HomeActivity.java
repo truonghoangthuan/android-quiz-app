96ffeeddc825cc5f73c58888443f622c69dd4956
truonghoangthuan <tthun32@gmail.com>
2022-02-09 19:56:17 +0700

    Get category from Firebase

--- app/src/main/java/com/thuan/quizapp/HomeActivity.java
+++ app/src/main/java/com/thuan/quizapp/HomeActivity.java
@@ -1,16 +1,49 @@
 package com.thuan.quizapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationHome);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.categoryBottomNav:
+                        fragment = CategoryFragment.newInstance();
+                        break;
+                    case R.id.rankingBottomNav:
+                        fragment = RankingFragment.newInstance();
+                        break;
+                }
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                assert fragment != null;
+                fragmentTransaction.replace(R.id.frameLayout, fragment);
+                fragmentTransaction.commit();
+                return true;
+            }
+        });
+        setDefaultFragment();
+    }
+
+    private void setDefaultFragment() {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, CategoryFragment.newInstance());
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/thuan/quizapp/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.thuan.quizapp.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
